ea1b798b58ed19e688470bc930aad1d04980cf50
Pugz <pugzfordayz132@gmail.com>
2019-12-01 15:14:13 -0500

    quick biome fixes

--- src/main/java/com/pugz/bloomful/core/registry/BiomeRegistry.java
+++ src/main/java/com/pugz/bloomful/core/registry/BiomeRegistry.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = "bloomful", bus = Mod.EventBusSubscriber.Bus.MOD)
 public class BiomeRegistry {
     public static Biome WISTERIA_FOREST = new WisteriaForestBiome(new Biome.Builder().surfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_DIRT_GRAVEL_CONFIG).precipitation(Biome.RainType.RAIN).category(Biome.Category.FOREST).depth(0.1F).scale(0.2F).temperature(0.7F).downfall(0.8F).waterColor(4159204).waterFogColor(329011).parent(null)).setRegistryName("wisteria_forest");
-    public static Biome WISTERIA_FOREST_HILLS = new WisteriaForestHillsBiome(new Biome.Builder().surfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_DIRT_GRAVEL_CONFIG).precipitation(Biome.RainType.RAIN).category(Biome.Category.FOREST).depth(0.45F).scale(0.3F).temperature(0.7F).downfall(0.8F).waterColor(4159204).waterFogColor(329011).parent(null)).setRegistryName("wisteria_forest");
+    public static Biome WISTERIA_FOREST_HILLS = new WisteriaForestHillsBiome(new Biome.Builder().surfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_DIRT_GRAVEL_CONFIG).precipitation(Biome.RainType.RAIN).category(Biome.Category.FOREST).depth(0.45F).scale(0.3F).temperature(0.7F).downfall(0.8F).waterColor(4159204).waterFogColor(329011).parent(null)).setRegistryName("wisteria_forest_hills");
 
     @SubscribeEvent
     public static void registerBiomes(RegistryEvent.Register<Biome> event) {
